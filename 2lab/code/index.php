fd884a6201b5cdf05df43bead92d64dc1ee65434
adyl <122738884+abusemepolly@users.noreply.github.com>
2024-03-08 16:05:07 +0200

    Add numeric variables display with browser line breaks
    Implemented the display of integer, floating-point variables, and the calculation of monthly expense difference. To ensure proper line breaks in the web browser output, utilized the `<br>` HTML tag instead of `\n`. This adjustment makes the output user-friendly when viewed in a browser, catering to the common scenario of PHP scripts being executed in a web environment.
    
    - Introduced an integer variable and displayed its value.
    - Added a floating-point variable and displayed its value.
    - Demonstrated arithmetic operation to output the number 12 without directly using it in the code.
    - Calculated and displayed the difference in monthly expenses using variables.
    
    Each output is followed by a `<br>` tag to ensure visibility and readability in web browsers.

--- 2lab/code/index.php
+++ 2lab/code/index.php
@@ -12,4 +12,23 @@ $order .= ', extra garlic sauce, fries and a Dobry Cola';
 
 
 // Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.";
+echo "\nYour order is: $very_bad_unclear_name." . "<br>";
+
+
+// 2nd Task
+
+$integerValue = 10; // Объявляем переменную с целочисленным значением
+echo $integerValue . "<br>"; // Выводим значение переменной на экран и делаем перевод строки
+
+$floatingValue = 3.14; // Объявляем переменную с дробным значением
+echo $floatingValue . "<br>";
+
+echo (6 + 6) . "<br>"; // Выводим результат арифметической операции, равный 12
+
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+$difference = $lastMonth - $thisMonth; // Рассчитываем разницу
+
+echo "Насколько больше я потратил в прошлом месяце, чем в этом месяце?" . "<br>" .
+    "В прошлом месяце я потратил $lastMonth, а в этом месяце я потратил $thisMonth." . "<br>" .
+    "Получается, что я потратил на $difference больше!" . "<br>";
